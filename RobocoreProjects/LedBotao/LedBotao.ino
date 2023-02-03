9a86c9b771857d3a0a27801e4a32bb9b26ef555e
MatthewBailerDemmer <matheusbailerdemmer@gmail.com>
2023-02-03 17:35:22 -0300

    Projeto LedBotao

---
+++ RobocoreProjects/LedBotao/LedBotao.ino
@@ -0,0 +1,19 @@
+const int ledPin = 13;
+const int Botao = 2;
+int estadoBotao = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  estadoBotao = digitalRead(Botao);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  estadoBotao = digitalRead(Botao);
+
+  if(estadoBotao == HIGH){
+    digitalWrite(ledPin, HIGH);
+  }else{
+    digitalWrite(ledPin, LOW);
+  }
+}
